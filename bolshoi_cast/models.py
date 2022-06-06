10de1ac0724f7a098071f6cf623b0b72757dd4a3
ekaterina <katarina-os@mail.ru>
2022-06-06 11:20:12 +0300

    Made database and wrote model Artist

--- bolshoi_cast/models.py
+++ bolshoi_cast/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=60)
+    surname = models.CharField(max_length=60)
+    sphere = models.CharField(max_length=60)
+    position = models.CharField(max_length=60)
+    photo = models.CharField(max_length=20000,default=50,null=True)
+
+    def __str__(self):
+        return f'{self.name},{self.surname},{self.sphere},{self.position}{self.photo}'
+
