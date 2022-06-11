4de907bd2807fea5cd80995226a89f3fa2fd1e8b
ekaterina <katarina-os@mail.ru>
2022-06-11 16:02:53 +0300

    Create admin panel

--- bolshoi_cast/admin.py
+++ bolshoi_cast/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Artist
 
-# Register your models here.
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ['id','name','realm','position']
+    list_editable = ['name','realm','position']
+    ordering = ['id']
+    list_per_page = 25
