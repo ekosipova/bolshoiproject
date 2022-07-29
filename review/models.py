141b02c768852710d4c042bcd01edbbddd283fb7
ekaterina <katarina-os@mail.ru>
2022-07-29 17:32:04 +0300

    Rewrote model, added max and min values for rating

--- review/models.py
+++ review/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.core.validators import MinLengthValidator
 from bolshoi_cast.models import Artist
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Feedbacks(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE,verbose_name='artist_name',default=1)
-    rating = models.PositiveIntegerField()
+    rating = models.PositiveIntegerField(default=1,max_length=1,validators=[MaxValueValidator(5),MinValueValidator(1)])
     feedback = models.TextField()
